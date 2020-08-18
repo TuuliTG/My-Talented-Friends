ce04aa5aced888b526f721ef614f308a9d21ac8b
Toivanen-Gripentrog Tuuli <tgtuuli@cubbli18-gold.local>
2020-08-18 22:46:12 +0300

    logout

--- src/main/java/projekti/securityConfiguration/ProductionSecurityConfiguration.java
+++ src/main/java/projekti/securityConfiguration/ProductionSecurityConfiguration.java
@@ -47,10 +47,10 @@ public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapte
         
         
                 http.logout()
-                .clearAuthentication(true)
-                .logoutUrl("/perform_logout")
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/frontpage")
                 .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
                 .invalidateHttpSession(true) 
                 ;
                 
